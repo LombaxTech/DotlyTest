1fdc13ea9067266430b6b8d241adfec7968321e4
Rakib <rakib@think-engineer.com>
2024-02-10 03:29:30 +0000

    test page

---
+++ pages/test.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { openai } from "@/openai";
+import axios from "axios";
+
+const url = `http://localhost:3000`;
+
+export default function Test() {
+  const [generation, setGeneration] = useState<any>("");
+
+  const generateSomething = async () => {
+    try {
+      console.log("trying to generate something");
+
+      let res = await axios.get(`${url}/api/get-completion`);
+      console.log(res);
+
+      setGeneration(res.data.result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="p-10 flex justify-center">
+      <div className="flex flex-col gap-4">
+        <h1>Test Page</h1>
+        <button className="btn" onClick={generateSomething}>
+          Generate something
+        </button>
+
+        {generation && <div className="">{generation}</div>}
+      </div>
+    </div>
+  );
+}
