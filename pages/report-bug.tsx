6631b1e2fe346a41c215943e8398402b6189a8f6
Rakib <rakib@think-engineer.com>
2024-02-25 18:24:08 +0000

    report a bug page

--- pages/report-bug.tsx
+++ pages/report-bug.tsx
@@ -1,7 +1,8 @@
 import { AuthContext } from "@/context/AuthContext";
-import { db } from "@/firebase";
+import { db, storage } from "@/firebase";
 import { addDoc, collection, doc } from "firebase/firestore";
-import React, { useContext, useState } from "react";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import React, { useContext, useRef, useState } from "react";
 
 export default function ReportBug() {
   const { user } = useContext(AuthContext);
@@ -11,11 +12,31 @@ export default function ReportBug() {
   const [success, setSuccess] = useState<any>(false);
   const [error, setError] = useState<any>(false);
 
+  const [file, setFile] = useState<any>(null);
+  const fileInputRef = useRef<any>(null);
+
   const sendReport = async () => {
     setSuccess(false);
     setError(false);
 
+    console.log("here is the file");
+    console.log(file);
+
     try {
+      let imageUrl;
+
+      // Upload image if image
+      if (file) {
+        const fileRef = `bugReports/${user.uid}/${file.name}`;
+
+        const storageRef = ref(storage, fileRef);
+        await uploadBytes(storageRef, file).then((snapshot) => {
+          console.log("Uploaded a blob or file!");
+        });
+
+        imageUrl = await getDownloadURL(ref(storage, fileRef));
+      }
+
       console.log(bugInputText);
       const newBug = {
         bugText: bugInputText,
@@ -24,6 +45,7 @@ export default function ReportBug() {
           id: user.uid,
           name: user.name,
         },
+        ...(imageUrl && { imageUrl }),
       };
 
       await addDoc(collection(db, "bug-reports"), newBug);
@@ -45,6 +67,22 @@ export default function ReportBug() {
         value={bugInputText}
         onChange={(e) => setBugInputText(e.target.value)}
       />
+
+      <input
+        type="file"
+        ref={fileInputRef}
+        onChange={(e: any) => setFile(e.target.files[0])}
+        // style={{ display: "none" }}
+      />
+      {file && (
+        <div className="relative">
+          <img src={URL.createObjectURL(file)} width="100" />
+          <div className="cursor-pointer" onClick={() => setFile(null)}>
+            Delete
+          </div>
+        </div>
+      )}
+
       <button className="btn" onClick={sendReport} disabled={!bugInputText}>
         Submit Report
       </button>
